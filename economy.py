84bf8ce02822c0d407b1db1131d6beff6d87ff7d
Finn <finn.pm.schaefer@gmail.com>
2025-02-24 23:36:39 -0500

    Update economy.py

--- economy.py
+++ economy.py
@@ -87,8 +87,6 @@ class Economy(commands.Cog):
                 await ctx.send("⚠️ You must bet on either a number or a color.")
                 return
             
-            
-            
         # Calculate the result
         if number is not None:
             if number < 0 or number > 36:
